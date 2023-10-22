d103d8b66197fbbd72554dd295e272102e9d6129
Jari Saari <jari.m.saari@gmail.com>
2023-10-22 20:11:37 +0300

    Älä kirjoita päälle skannatessa.

--- skannaa-aukeama.sh
+++ skannaa-aukeama.sh
@@ -1,8 +1,17 @@
 #echo $(($1+1))
 
-scanimage -d pixma:04A91827_2A949A --resolution 300 --format png >images/$1.png
-convert images/$1.png -rotate 180 images/$1.png
-python clinterface.py -i $1.png -s 0 -r 1
+page=$1
+if [ ! -f images/$page.png ]; then
+    #echo "File not found!"
+    scanimage -d pixma:04A91827_2A949A --resolution 300 --format png >images/$page.png
+    convert images/$page.png -rotate 180 images/$page.png
+    python clinterface.py -i $page.png -s 0 -r 1
+fi
+
 read -p "Press enter to scan page $(($1+1))"
-scanimage -d pixma:04A91827_2A949A --resolution 300 --format png >images/$(($1+1)).png
-python clinterface.py -i $(($1+1)).png -s 0 -r 1
+
+page=$(($1+1))
+if [ ! -f images/$page.png ]; then
+    scanimage -d pixma:04A91827_2A949A --resolution 300 --format png >images/$page.png
+    python clinterface.py -i $page.png -s 0 -r 1
+fi
