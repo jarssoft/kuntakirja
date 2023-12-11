c2680f3cec0ea6e363849d8706f1532ff41f69a9
Jari Saari <jari.m.saari@gmail.com>
2023-12-11 19:52:32 +0200

    Lisättiin backendiin kylähaku ja talohakuun sort, reverse ja offset.

--- webapp-backend/index.js
+++ webapp-backend/index.js
@@ -14,12 +14,43 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  console.log(request);
+  //console.log(request);
   const pattern = request.query["pattern"].toLowerCase();
+  const offset = request.query["offset"]
+    ? parseInt(request.query["offset"])
+    : 0;
+  const sort = request.query["sort"] ? request.query["sort"] : "tontti";
+  const reverse = request.query["reverse"] ? 1 : -1;
   const selected = asukkaat
     .filter((talo) =>
-      (talo["tontti"] + " " + talo["kyla"]).toLowerCase().includes(pattern)
+      (talo["tontti"] + " " + talo["kyla"] + " " + talo["rakennusvuosi"])
+        .toLowerCase()
+        .includes(pattern)
     )
+    .sort((talo1, talo2) => {
+      console.log(talo1["pinta-ala"]);
+      if (sort === "tontti") {
+        return (talo1["tontti"] < talo2["tontti"] ? 1 : -1) * reverse;
+      }
+      return (talo1[sort] - talo2[sort]) * reverse;
+    })
+    .slice(offset, offset + 30);
+  response.json(selected);
+});
+
+function onlyUnique(value, index, array) {
+  return array.indexOf(value) === index;
+}
+
+app.get("/api/kylat", (request, response) => {
+  //console.log(request);
+  const selected = asukkaat
+    .map((talo) => talo["kyla"])
+    .filter(onlyUnique)
+    .filter((kyla) => kyla != null)
+    .sort((kyla1, kyla2) => {
+      return kyla1 > kyla2 ? 1 : -1;
+    })
     .slice(0, 30);
   response.json(selected);
 });
